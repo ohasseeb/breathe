8e2e3a8d46d6a49d328ec244f0614e7a6ca2d9ba
ohasseeb <hasseeb.obaid.2016@gmail.com>
2025-12-22 00:08:24 -0800

    Added some styling to duration

--- app/BoxBreathing/Duration.tsx
+++ app/BoxBreathing/Duration.tsx
@@ -1,11 +1,30 @@
 import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
-export default function Duration() {
+// Takes in Seconds as Props , if no prop then custom is selected
+
+type DurationProps = {
+  seconds?: number;
+};
+
+export default function Duration({ seconds }: DurationProps) {
   return (
     <View>
-      <Text>Duration Component</Text>
-      <Link href="./Action"> Go to Action</Link>
+      <View className="items-center justify-center">
+        <Text className="text-5xl "> Box Breathing</Text>
+
+        <Text className=" text-5xl">
+          {seconds ? seconds : "Custom"} Duration{" "}
+        </Text>
+      </View>
+
+      <Text> Box Seconds {seconds ? seconds : ""} </Text>
+      <Text> Durations {seconds ? seconds : ""} </Text>
+      {/*  For Duration Add a picker for seconds / minutes */}
+
+      <View className="items-center justify-center">
+        <Link href="./Action"> Start</Link>
+      </View>
     </View>
   );
 }
