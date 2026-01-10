fdc284a4ed1e15d1e7322b7258e32cf3076535d8
ohasseeb <hasseeb.obaid.2016@gmail.com>
2026-01-09 17:12:42 -0800

    Style updates for Menu
    Style updates for Menu

--- app/BoxBreathing/Duration.tsx
+++ app/BoxBreathing/Duration.tsx
@@ -1,12 +1,15 @@
 import { Picker } from "@react-native-picker/picker";
-import { Link, useLocalSearchParams } from "expo-router";
+import { useLocalSearchParams, useRouter } from "expo-router";
 import { useEffect, useState } from "react";
 import { Text, TextInput, TouchableOpacity, View } from "react-native";
 import BackgroundView from "../components/BackgroundView";
+import StyledButton from "../components/StyledButton";
 // Takes in Seconds as Props , if no prop then custom is selected
 
 export default function Duration() {
   const { boxSeconds } = useLocalSearchParams();
+
+  const router = useRouter();
 
   // CONSTANTS
   const durationOptions = ["Holds", "Minutes"];
@@ -15,7 +18,7 @@ export default function Duration() {
   const isCustom = boxSeconds === "Custom";
 
   // STATES
-  const [durationType, setDurationType] = useState(durationOptions[MINUTES]);
+  const [durationType, setDurationType] = useState(durationOptions[HOLDS]);
   const [customSeconds, onChangeCustomSeconds] = useState("");
   const [customDuration, onChangeCustomDuration] = useState("");
   const [openPicker, setOpenPicker] = useState(false);
@@ -36,22 +39,23 @@ export default function Duration() {
     return actionNavigationObj;
   }
 
-  console.log("Box Seconds Params:", boxSeconds);
   return (
     <BackgroundView className="h-full">
       {/* Text displaying the box breathing duration */}
       <View className="items-center justify-center">
-        <Text className="text-5xl color-light-200"> Box Breathing</Text>
+        <Text className="text-5xl color-light-200 font-semibold">
+          Box Breathing
+        </Text>
 
-        <Text className=" text-3xl color-light-300">
+        <Text className=" text-header-secondary color-light-300 font-medium">
           {boxSeconds ? boxSeconds : "Custom"} Second Holds
         </Text>
       </View>
 
       {/* Text displaying the box breathing duration in seconds */}
       <View className="flex flex-row items-center justify-center mt-20">
-        <Text className="text-subheader-secondary">Box Seconds </Text>
-        <Text className="text-subheader-secondary">
+        <Text className="text-header-secondary font-medium">Box Seconds: </Text>
+        <Text className="text-header-secondary">
           {!isCustom ? boxSeconds : " "}
         </Text>
         {isCustom && (
@@ -66,15 +70,17 @@ export default function Duration() {
 
       {/* Text displaying the box breathing duration type */}
       <View className="flex flex-row items-center justify-center mt-10">
-        <Text className="text-subheader-secondary"> Duration: </Text>
+        <Text className="text-header-secondary"> Duration: </Text>
         <TextInput
-          className="border border-gray-300 rounded p-2 text-form-value w-20"
-          placeholder="Enter custom duration"
+          className="border border-gray-300 rounded p-2 text-header-secondary w-19"
+          placeholder="10" //Enter custom duration
           onChangeText={onChangeCustomDuration}
           value={customDuration}
         />
         <TouchableOpacity onPress={togglePicker}>
-          <Text className="text-subheader-secondary ml-2">{durationType}</Text>
+          <Text className="text-header-secondary ml-2 border border-gray-100">
+            {durationType}
+          </Text>
         </TouchableOpacity>
       </View>
 
@@ -92,7 +98,7 @@ export default function Duration() {
           </Picker>
           <TouchableOpacity onPress={togglePicker}>
             <View className="items-center justify-center">
-              <Text className="text-subheader-secondary border border-gray-300 rounded p-2 mt-2">
+              <Text className="text-header-secondary border border-gray-300 rounded p-2 mt-2">
                 Close
               </Text>
             </View>
@@ -102,16 +108,20 @@ export default function Duration() {
 
       {/* Text displaying the Start button to start navigation */}
       <View className="items-center justify-center mt-20">
-        <Link
-          className="text-subheader-primary"
-          href={navigateToAction(
-            Number(boxSeconds) ? Number(boxSeconds) : Number(customSeconds),
-            customDuration ? Number(customDuration) : Number(boxSeconds),
-            durationType
-          )}
+        <StyledButton
+          onPress={() =>
+            router.push(
+              navigateToAction(
+                Number(boxSeconds) ? Number(boxSeconds) : Number(customSeconds),
+                customDuration ? Number(customDuration) : Number(boxSeconds),
+                durationType
+              )
+            )
+          }
+          buttonColor="mellowGreen"
         >
-          Start
-        </Link>
+          <Text className="text-header-secondary"> Start</Text>
+        </StyledButton>
       </View>
     </BackgroundView>
   );
